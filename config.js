ba37294cea474f44b0336d359a0c06f37b67f56a
Harshitcorder <104720724+Harshitcorder@users.noreply.github.com>
2023-05-20 21:56:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh_KlBd_7bIeAF1rltBibHNJAg3sLwuAM",
+  authDomain: "c71-7cc64.firebaseapp.com",
+  projectId: "c71-7cc64",
+  storageBucket: "c71-7cc64.appspot.com",
+  messagingSenderId: "596981113726",
+  appId: "1:596981113726:web:3a045f298cf982f2b2892f"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
